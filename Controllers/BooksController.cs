8fdcf04e45c6547784d0a3739ca2c3214c0c6d6f
Akif-Imran <raiden3322@gmail.com>
2022-07-12 12:12:37 +0500

    BooksController CRUD operations completed.

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using myBooks.Data.Services;
 using myBooks.Data.ViewModels;
 
@@ -20,11 +21,42 @@ namespace myBooks.Controllers
       _bookService = bookService;
     }
 
+    [HttpGet(template: "get-book-by-id")]
+    public IActionResult GetBook([FromQuery] int id)
+    {
+      var book = _bookService.GetBook(id);
+      if (book == null) return NoContent();
+      return Ok(book);
+    }
+
+    [HttpGet(template: "get-all-books")]
+    public IActionResult GetAllBooks()
+    {
+      var books = _bookService.GetAllBooks();
+      if (books.Count == 0) return NoContent();
+      return Ok(books);
+    }
+
     [HttpPost(template: "add-book")]
     public IActionResult AddBook([FromBody] BookViewModel book)
     {
       _bookService.AddBook(book: book);
       return Ok();
+    }
+
+    [HttpPut(template: "update-book-by-id")]
+    public IActionResult UpdateBookById([FromQuery, BindRequired] int id, [FromBody] BookViewModel book)
+    {
+      var updatedBook = _bookService.UpdateBookById(id, book);
+      if (updatedBook == null) return NoContent();
+      return Ok(updatedBook);
+    }
+
+    [HttpDelete(template: "delete-book-by-id")]
+    public IActionResult DeleteBookById([FromQuery] int id)
+    {
+      bool result = _bookService.DeleteBookById(id);
+      return result ? Ok(new { message = "Book Deleted" }) : NoContent();
     }
   }
 }
--- Data/Services/BookService.cs
+++ Data/Services/BookService.cs
@@ -9,7 +9,7 @@ namespace myBooks.Data.Services
 {
   public class BookService
   {
-    private AppDbContext _context;
+    private readonly AppDbContext _context;
 
     public BookService(AppDbContext context)
     {
@@ -32,6 +32,41 @@ namespace myBooks.Data.Services
       };
       _context.Books.Add(_book);
       _context.SaveChanges();
+    }
+
+    public List<Book> GetAllBooks()
+    {
+      return _context.Books.ToList();
+    }
+
+    public Book GetBook(int id)
+    {
+      return _context.Books.Find(id);
+    }
+
+    public Book UpdateBookById(int id, BookViewModel newBookRecord)
+    {
+      var book = _context.Books.Find(id);
+      if (book == null) return null;
+      book.Title = newBookRecord.Title;
+      book.Description = newBookRecord.Description;
+      book.IsRead = newBookRecord.IsRead;
+      book.DateRead = newBookRecord.DateRead;
+      book.Rating = newBookRecord.Rating;
+      book.Genre = newBookRecord.Genre;
+      book.Author = newBookRecord.Author;
+      book.CoverUrl = newBookRecord.CoverUrl;
+      _context.SaveChanges();
+      return book;
+    }
+
+    public bool DeleteBookById(int id)
+    {
+      var book = _context.Books.Find(id);
+      if (book == null) return false;
+      _context.Books.Remove(book);
+      _context.SaveChanges();
+      return true;
     }
   }
 }
