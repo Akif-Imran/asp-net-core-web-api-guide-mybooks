2e39725c493f341e6742c69cbb009a7ee0f46e73
Akif-Imran <raiden3322@gmail.com>
2022-07-13 18:48:29 +0500

    Code CleanUp AppDbInitializer.cs file.

--- Data/AppDbInitializer.cs
+++ Data/AppDbInitializer.cs
@@ -10,38 +10,34 @@ namespace myBooks.Data
   {
     public static void Seed(IApplicationBuilder applicationBuilder)
     {
-      using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
-      {
-        var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
-        if (!context.Books.Any())
+      using var serviceScope = applicationBuilder.ApplicationServices.CreateScope();
+      var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+      if (context == null || context.Books.Any()) return;
+      context.Books.AddRange(new Book()
         {
-          context.Books.AddRange(new Book()
-            {
-              Title = "1st Book Title",
-              Description = "1st Book Description",
-              IsRead = true,
-              DateRead = DateTime.Now.AddDays(-10),
-              Rating = 4,
-              Genre = "Biography",
-              Author = "First Author",
-              CoverUrl = "https:www.example.com/.....",
-              DateAdded = DateTime.Now
-            },
-            new Book()
-            {
-              Title = "2nd Book Title",
-              Description = "2nd Book Description",
-              IsRead = false,
-              DateRead = DateTime.Now.AddDays(-10),
-              Genre = "Biography",
-              Author = "First Author",
-              CoverUrl = "https:www.example.com/.....",
-              DateAdded = DateTime.Now
-            }
-          );
-          context.SaveChanges();
+          Title = "1st Book Title",
+          Description = "1st Book Description",
+          IsRead = true,
+          DateRead = DateTime.Now.AddDays(-10),
+          Rating = 4,
+          Genre = "Biography",
+          Author = "First Author",
+          CoverUrl = "https:www.example.com/.....",
+          DateAdded = DateTime.Now
+        },
+        new Book()
+        {
+          Title = "2nd Book Title",
+          Description = "2nd Book Description",
+          IsRead = false,
+          DateRead = DateTime.Now.AddDays(-10),
+          Genre = "Biography",
+          Author = "First Author",
+          CoverUrl = "https:www.example.com/.....",
+          DateAdded = DateTime.Now
         }
-      }
+      );
+      context.SaveChanges();
     }
   }
 }
